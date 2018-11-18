5a80a0dc38531e82ef9c0f25e7b9fa3970982c14
Huseyin Aygun <huseyinaygun@Huseyin-iMac.local>
2018-11-18 15:48:31 +0300

    Kendi olusturduğum gem file ekleme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,7 @@ module ApplicationHelper
       		content_tag(:p, greeting, class: "source-greeting")    
       	end  
 	end
+	def copyright_generator
+		HuseyinAygunTool::Renderer.copyright 'Hüseyin Aygün', 'All rights reserved'
+	end
 end
