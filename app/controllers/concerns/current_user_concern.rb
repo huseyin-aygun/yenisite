15b86a28a697e4d3a9dd12e767045737b0b22d6b
Huseyin Aygun <huseyinaygun@Huseyin-iMac.local>
2018-11-23 20:43:58 +0300

    Kullanıcı yetkileri atamaları

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,9 +5,11 @@ module CurrentUserConcern
 		super || guest_user
 	end
 	def guest_user
-		OpenStruct.new(name: "Guest User", 
-					   first_name: "Guest", 
-					   last_name: "User", 
-					   email: "guest@.com")
+		guest = GuestUser.new
+		guest.name = "Guest User"
+		guest.first_name = "Guest"
+		guest.last_name = "User"
+		guest.email = "guest@com"
+		guest 
 	end
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,8 @@
 class PortfoliosController < ApplicationController
 	before_action :set_portfolio_item, only: [:edit, :show,  :update, :destroy]
 	layout "portfolio"
+    access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+
 	def index
 		@portfolio_items = Portfolio.all 
 	end
@@ -36,7 +38,7 @@ class PortfoliosController < ApplicationController
     end
 
  	def show
- 		binding.pry
+ 		 
  	end 
  	def destroy
 
