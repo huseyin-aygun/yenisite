434234c72f88a5fd291b310e866fc40ecbb89ff2
Huseyin Aygun <huseyinaygun@Huseyin-iMac.local>
2018-12-07 18:43:20 +0300

    Sortable JS eklendi

--- Gemfile
+++ Gemfile
@@ -53,7 +53,6 @@ end
 
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'jquery', '~> 0.0.1'
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.5'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
-//= require jquery-ui
 //= require activestorage
+//= require jquery3
+//= require jquery-ui
 //= require html.sortable
 //= require turbolinks
 //= require_tree .
+//= require popper
+//= require bootstrap-sprockets
